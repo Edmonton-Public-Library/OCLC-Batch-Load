9dbb8af4f2fc603c737f6b50efdf6bc967385970
Andrew Nisbet <anisbet@epl.ca>
2012-10-04 09:52:29 -0600

    Added the wrapper shell scripts for cron to repo.

--- Makefile
+++ Makefile
@@ -1,17 +1,25 @@
 # copies most rescent files from eplapp for updating to git.
 SERVER=eplapp.library.ualberta.ca
 USER=sirsi
+# this is the test directory. Production is in cronjobscripts.
+# this allows us to move test scripts to the server without
+# affecting the existing production code.
 REMOTE=~/Unicorn/EPLwork/anisbet/OCLC/
 LOCAL=~/projects/oclc/
 APP=oclc.pl
 FTP=ftp.pl
+WRAPPER_CANCELS=oclc_cancels.sh
+WRAPPER_MIXED=oclc_mixed.sh
 
 put: test
 	scp ${LOCAL}${APP} ${USER}@${SERVER}:${REMOTE}
+	scp ${LOCAL}${WRAPPER_CANCELS} ${USER}@${SERVER}:${REMOTE}
+	scp ${LOCAL}${WRAPPER_MIXED} ${USER}@${SERVER}:${REMOTE}
 get:
 	scp ${USER}@${SERVER}:${REMOTE}${APP} ${LOCAL}
 test:
 	perl -c ${APP}
+# these rules are for the FTP test script only.
 putftp: test_ftp
 	scp ${LOCAL}${FTP} ${USER}@${SERVER}:${REMOTE}
 test_ftp:
---
+++ oclc_mixed.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+CRON_DIR="/s/sirsi/Unicorn/EPLwork/cronjobscripts"
+OCLC_DIR="$CRON_DIR/OCLC"
+source $CRON_DIR/setscriptenvironment.sh
+# this file deliberately not called .log because oclc.pl -w deletes those.
+SUMMARY_LOG="/s/sirsi/Unicorn/EPLwork/cronjobscripts/OCLC/summary.txt"
+
+# clean up from the last run
+MSG="["`date`"] started OCLC directory cleaned up"
+echo $MSG > $SUMMARY_LOG
+echo      >> $SUMMARY_LOG
+$OCLC_DIR/oclc.pl -w
+
+# create list of Cancels for the last month
+MSG="["`date`"] started adds/deletes list"
+echo $MSG >> $SUMMARY_LOG
+echo      >> $SUMMARY_LOG
+$OCLC_DIR/oclc.pl -A
+
+# ftp the lists to OCLC
+MSG="["`date`"] ftp'ed files to OCLC"
+echo $MSG >> $SUMMARY_LOG
+echo      >> $SUMMARY_LOG
+$OCLC_DIR/oclc.pl -f
+
+MSG="["`date`"] generated summary"
+echo $MSG >> $SUMMARY_LOG
+echo      >> $SUMMARY_LOG
+
+# report what you did
+cat $SUMMARY_LOG | mailx -s "OCLC Mixed Upload" "anisbet@epl.ca"
